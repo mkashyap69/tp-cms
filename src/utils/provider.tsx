a3982adadc01e122632979086752c1d49b6b2254
mk671326 <mk671326@gamil.com>
2023-09-08 17:35:26 +0530

    Added unnecessary packages

--- src/utils/provider.tsx
+++ src/utils/provider.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 import StyledComponentsRegistry from '@/lib/AntdRegistry';
 import theme from '@/theme/themeConfig';
 import { ConfigProvider } from 'antd';
@@ -13,11 +12,9 @@ const queryClient = new QueryClient();
 function Providers({ children }: React.PropsWithChildren) {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <ReactQueryStreamedHydration> */}
       <StyledComponentsRegistry>
         <ConfigProvider theme={theme}>{children}</ConfigProvider>
       </StyledComponentsRegistry>
-      {/* </ReactQueryStreamedHydration> */}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
